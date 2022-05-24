91290609b82abfbe2c1dec03adbfad42e1e8073d
Kevin Sana <kevin.sana@outlook.com>
2022-05-24 11:02:04 -0500

    added form validation and error handling

--- index.php
+++ index.php
@@ -1,20 +1,60 @@
 <?php include 'inc/header.php'; ?>
 
+<?php
+$name  = $email = $body = '';
+$nameErr = $emailErr = $bodyErr = '';
+
+// Form submit
+if (isset($_POST['submit'])) {
+    // Validate name
+    if (empty($_POST['name'])) {
+        $nameErr = 'Name is required';
+    } else {
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    // Validate email
+    if (empty($_POST['email'])) {
+        $emailErr = 'Email is required';
+    } else {
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    }
+
+    // Validate body
+    if (empty($_POST['body'])) {
+        $bodyErr = 'feedback is required';
+    } else {
+        $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+}
+
+?>
+
+
 <img src="/php-feedback-form/img/shiba.jpg" class="w-25 mb-3 rounded" alt="logo">
 <h2>Feedback</h2>
 <p class="lead text-center">Leave some feedback for Kevin</p>
-<form action="" class="mt-4 w-75">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Enter your name">
+        <input type="text" class="form-control <?php echo $nameErr ? 'is-invalid' : null; ?>" id="name" name="name" placeholder="Enter your name">
+        <div class="invalid-feedback">
+            <?php echo $nameErr; ?>
+        </div>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+        <input type="email" class="form-control <?php echo $emailErr ? 'is-invalid' : null; ?>" id="email" name="email" placeholder="Enter your email">
+        <div class="invalid-feedback">
+            <?php echo $emailErr; ?>
+        </div>
     </div>
     <div class="mb-3">
         <label for="body" class="form-label">Feedback</label>
-        <textarea name="body" id="body" class="form-control" placeholder="Enter your feedback"></textarea>
+        <textarea name="body" id="body" class="form-control <?php echo $bodyErr ? 'is-invalid' : null; ?>" name="body" placeholder="Enter your feedback"></textarea>
+        <div class="invalid-feedback">
+            <?php echo $bodyErr; ?>
+        </div>
     </div>
     <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
