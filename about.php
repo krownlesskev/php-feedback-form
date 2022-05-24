043a3aa4544f7db2a84708ccf68d5b29fe18ce1b
Kevin Sana <kevin.sana@outlook.com>
2022-05-24 10:17:22 -0500

    added about and feedback page

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php include 'inc/header.php'; ?>
+<h2>About</h2>
+<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Ullam numquam consectetur repellat, possimus laboriosam mollitia,
+    earum, deleniti suscipit sint quia dolorem quas voluptatem? Quos
+    tempore veniam, sunt odit magnam quidem?
+</p>
+</div>
+</main>
+
+
+<?php include 'inc/footer.php'; ?>
